e5acf52d0a7b0d6a46b2519a6af136c9b51b8efc
Ruslan Zykov <r.v.zykov@rambler.ru>
2023-02-11 11:51:38 +0400

    feat:валидация форм

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const editProfileButton = document.querySelector('.profile__edit-button');
 const addNewCardButton = document.querySelector('.profile__add-button');
 const profileName = document.querySelector('.profile__name');
 const profileAboutMe = document.querySelector('.profile__about-me');
-const closeButtons = document.querySelectorAll('.popup__close-button'); //все крестики проектаы
+const closeButtons = document.querySelectorAll('.popup__close-button'); // todo: все крестики проекта
 //Переменные попапа для редактирования профиля
 const popupEdit = document.querySelector('.popup_type_edit');
 const formElementEdit = document.querySelector('.popup__container_type_edit');
@@ -29,11 +29,15 @@ function openPopup(popupElement) {
 //ФУНКЦИЯ ДЛЯ ЗАКРЫТИЯ ВСПЛЫВАЮЩЕГО ОКНА
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
+  const closePopupDeleteValid = Array.from(popupElement.querySelectorAll(config.inputSelector));
+  closePopupDeleteValid.forEach(function (item) {
+    hideInputError(popupElement, item);
+  });
 }
 
 //ФУНКЦИЯ ДЛЯ ОТПРАВКИ ДАННЫХ ПО КНОПКЕ СОХРАНИТЬ ПРИ ИЗМЕНЕНИИ ПРОФИЛЯ
 function handleFormSubmitEditProfile(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки.
+  evt.preventDefault(); // todo: эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки
   profileName.textContent = nameInput.value;
   profileAboutMe.textContent = jobInput.value;
   closePopup(popupEdit);
@@ -66,12 +70,12 @@ function handleFormSubmitAddNewCard(evt) {
 
 //ФУНКЦИЯ ДЛЯ ВИЗУАЛИЗАЦИИ НОВЫХ КАРТОЧЕК ИЗ МАССИВА
 function renderCardArray() {
-    initialCards.forEach(function (initialCard) {
-      const name = initialCard.name;
-      const link = initialCard.link;
-      const card = createCard(name, link);
+  initialCards.forEach(function (initialCard) {
+    const name = initialCard.name;
+    const link = initialCard.link;
+    const card = createCard(name, link);
     galery.append(card);
-  })
+  });
 }
 
 renderCardArray();
@@ -114,13 +118,29 @@ function handleClickCard(evt) {
   }
 }
 
-//ФУНКЦИЯ ДЛЯ ЗАКРЫТИЯ ПОПАПОВ ПО ESC
-document.addEventListener('keydown', function (evt) {
+//ФУНКЦИЯ ДЛЯ ЗАКРЫТИЯ ПОПАПОВ ПО КНОПКЕ ESC
+function closePopupByClickEsq(evt) {
+  const openedPopup = document.querySelector('.popup_opened'); // todo: нашли открытый попап
   if (evt.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_opened'); //нашли открытый попап
-    closePopup(openedPopup);
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
   }
-});
+}
+
+//ФУНКЦИЯ ДЛЯ ЗАКРЫТИЯ ПОПАПОВ ПРИ КЛИКЕ ВНЕ ОКНА
+function closePopupByClickOutPopup(evt) {
+  const openedPopup = document.querySelector('.popup_opened'); // todo: нашли открытый попап
+  if (evt.target === openedPopup) {
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+}
+
+document.addEventListener('mousedown', closePopupByClickOutPopup);
+
+document.addEventListener('keydown', closePopupByClickEsq);
 
 editProfileButton.addEventListener('click', function () {
   nameInput.value = profileName.textContent;
@@ -135,7 +155,7 @@ addNewCardButton.addEventListener('click', function () {
 });
 
 closeButtons.forEach(function (button) {
-  const popup = button.closest('.popup'); // находим 1 раз ближайший к крестику попап
+  const popup = button.closest('.popup'); // todo: находим 1 раз ближайший к крестику попап
   button.addEventListener('click', function () {
     closePopup(popup);
   });
